5dfb2bf82331bbe90595f72b75c37f348511df2f
ConnorMoon <cgm19353@uga.edu>
2023-11-14 16:59:22 -0500

    demo-7-done

--- demos/section-7/Demo7/Assets/MirrorMovement.cs
+++ demos/section-7/Demo7/Assets/MirrorMovement.cs
@@ -10,18 +10,11 @@ public class MirrorMovement : MonoBehaviour
     void Update()
     {
         // Calculate the distance between the player and the mirror
-        Vector3 playerToMirror = mirror.position - playerTarget.position;
+        Vector3 localPlayer = mirror.InverseTransformPoint(playerTarget.position);
 
-        // Reflect the player's position across the mirror plane
-        Vector3 reflectedPlayerPos = mirror.position + Vector3.Reflect(playerToMirror, mirror.forward);
+        transform.position = mirror.TransformPoint(new Vector3(localPlayer.x, localPlayer.y, -localPlayer.z));
 
-        // Set the mirror's position to the reflected player position
-        playerTarget.position = reflectedPlayerPos;
-
-        // Calculate the rotation difference between the player and the mirror
-        Quaternion relativeRotation = Quaternion.Inverse(playerTarget.rotation) * mirror.rotation;
-
-        // Apply the reflected rotation to the player
-        playerTarget.rotation = Quaternion.LookRotation(-mirror.forward, mirror.up) * relativeRotation;
+        Vector3 lookatmirror = mirror.TransformPoint(new Vector3(-localPlayer.x, localPlayer.y, localPlayer.z));
+        transform.LookAt(lookatmirror);
     }
 }
